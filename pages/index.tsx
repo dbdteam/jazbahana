929f971e5d34d126a404f6189d08769e8e63a0c8
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-07-26 05:34:24 +0600

    feat(pages): authentication check on index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,38 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Hero from "../components/Hero";
-import Layout from "../components/Layout";
 import WhatIsJazbahana from "../components/WhatIsJazbahana";
-import Features from '../components/Features'
+import Features from "../components/Features";
+import { useEffect, useState } from "react";
+import { supabase } from "../utils/supabaseClient";
+import Auth from "../components/Auth";
 
 const Home: NextPage = () => {
+  const [session, setSession] = useState<any>(null);
+
+  useEffect(() => {
+    setSession(supabase.auth.session());
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
   return (
-    <Layout>
+    <>
       <Head>
         <title>Home | Jazbahana</title>
       </Head>
-      {/* Landing Page */}
-      <Hero />
-      <WhatIsJazbahana />
-      <Features />
-    </Layout>
+      {!session ? (
+        <Auth />
+      ) : (
+        <main>
+          <Hero />
+          <WhatIsJazbahana />
+          <Features />
+        </main>
+      )}
+    </>
   );
 };
 
