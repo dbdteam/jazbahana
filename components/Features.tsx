c782ab7df8b31531dcf22c919b80ee5888ce9655
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-07-25 19:45:36 +0600

    feat(*): update features

--- components/Features.tsx
+++ components/Features.tsx
@@ -4,39 +4,48 @@ function Features() {
   return (
     <div className="text-center my-24">
       <h1 className="text-secondary pb-5 font-bold">Features</h1>
-      <div className="grid grid-cols-1 my-12 mx-10">
+      <div className="grid grid-cols-1 my-12 max-w-[1080px] mx-auto">
         <div className="feature">
           <Image
+            className="rounded-md"
             src="/images/features/notes.png"
-            width={330}
+            width={190}
             height={250}
-            alt="solve feature"
+            alt="Notes Feature"
           />
           <p>
-            It solves the task by opening a chat-like room <br /> with an invite link,
-            that is sent to people you ask notes from
+            It solves the task by opening a chat-like room <br /> with an invite
+            link, that is sent to people you ask notes from
           </p>
         </div>
 
         <div className="feature">
-          <p className="hidden sm:flex">Notes are uploaded via file-select form.</p>
+          <p className="hidden sm:flex">
+            Notes are uploaded via file-select form. They can be images, PDFs
+            and Word documents.
+          </p>
           <Image
+            className="rounded-md"
             src="/images/features/form.png"
             width={300}
             height={250}
             alt="Upload Notes Feature"
           />
-          <p className="sm:hidden">Notes are uploaded via file-select form.</p>
+          <p className="sm:hidden">
+            Notes are uploaded via file-select form. They can be images, PDFs
+            and Word documents.
+          </p>
         </div>
 
         <div className="feature">
           <Image
-            src="/images/features/form.png"
-            width={300}
+            className="rounded-md"
+            src="/images/features/jazbapoint.png"
+            width={250}
             height={250}
-            alt="Upload Notes Feature"
+            alt="Economy Feature"
           />
-          <p>Notes are uploaded via file-select form.</p>
+          <p>Economy. Jazbahana offers its own currency called `JazbaPoint`</p>
         </div>
       </div>
     </div>
