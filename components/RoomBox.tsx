5f6a41b9917de8f7b5cc1519b8c31b853871088b
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-07-27 01:10:03 +0600

    feat(*): match rooms.json keys with db column names

--- components/RoomBox.tsx
+++ components/RoomBox.tsx
@@ -1,22 +1,25 @@
 import Image from "next/image";
 import { IoPeople } from "react-icons/io5";
+import relativeTime from "../utils/relativeTime";
 
 interface RoomParams {
-  author: string;
-  title: string;
+  id: string;
+  host: string;
+  created_at: Date;
+  name: string;
   description: string;
   participants: number;
   topics: string;
-  date: string;
 }
 
 const RoomBox = ({ room }: { room: RoomParams }) => {
   const iconStyles = { width: 24, height: 24 };
+
   return (
-    <div className="max-w-[720px] mx-auto my-10 bg-gray-100 rounded-md p-5">
+    <div className="max-w-[720px] mx-auto my-10 bg-dark rounded-md p-5">
       <div className="flex items-center justify-between">
-        <a href={`#${room.author}`} className="flex items-center gap-2">
-          <div className="border-2 border-primary rounded-full">
+        <a href={`#${room.host}`} className="flex items-center gap-2">
+          <div className="border-2 border-primary rounded-full h-8">
             <Image
               src="/images/avatar.svg"
               alt="Avatar"
@@ -24,19 +27,18 @@ const RoomBox = ({ room }: { room: RoomParams }) => {
               height={28}
             />
           </div>
-          <span>@{room.author}</span>
+          <span>@{room.host}</span>
         </a>
-        <span>{room.date}</span>
+        <span>Created {relativeTime(room.created_at)}</span>
       </div>
       <div className="border-b-2 py-4">
-        <a href={`#room-${room.title}`} className="text-2xl">
-          {room.title}
+        <a href={`#room-${room.name}`} className="text-2xl">
+          {room.name}
         </a>
         <p>{room.description}</p>
       </div>
       <p className="flex items-center gap-2 pt-4">
-        <IoPeople style={iconStyles} /> {room.participants}{" "}
-        participants
+        <IoPeople style={iconStyles} /> {room.participants} participants
       </p>
     </div>
   );
--- pages/rooms.tsx
+++ pages/rooms.tsx
@@ -3,13 +3,25 @@ import RoomBox from "../components/RoomBox";
 import rooms from "../data/rooms.json";
 
 const Rooms = () => {
+  // const [rooms, setRooms] = useState<any>([]);
+
+  // useEffect(() => {
+  //   fetchRooms();
+  // }, []);
+
+  // const fetchRooms = async () => {
+  //   let { data: rooms, error } = await supabase.from("rooms").select("*");
+  //   if (error) console.log("error", error);
+  //   else setRooms(rooms);
+  // };
+
   return (
     <div>
       <Head>
         <title>Rooms | Jazbahana</title>
       </Head>
-      {rooms.map((room) => (
-        <RoomBox key={room.title} room={room} />
+      {rooms.map((room: any) => (
+        <RoomBox key={room.name} room={room} />
       ))}
     </div>
   );
