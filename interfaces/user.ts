c66c907fca86ef3c85f175d5d455cc56a07d0983
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-07-26 23:07:02 +0600

    feat(interfaces): better typing

--- interfaces/user.ts
+++ interfaces/user.ts
@@ -1,6 +1,14 @@
 export default interface User {
+  id: string;
+  updated_at: Date;
   username: string;
   bio: string;
   avatar_url: string;
   balance?: number;
+}
+
+export interface EditableCredentials {
+  username: string;
+  bio: string;
+  avatar_url: string;
 }
