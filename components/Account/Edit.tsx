2d7d9224e405f10e8d6f92c695f59b0374a1a5e9
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-07-26 23:06:25 +0600

    fix(components): edit profile - fix data fetch and improve styling

--- components/Account/Edit.tsx
+++ components/Account/Edit.tsx
@@ -1,37 +1,60 @@
 import { useState, useEffect } from "react";
 import { supabase } from "../../utils/supabaseClient";
 import Avatar from "../Avatar";
-import User from "../../interfaces/user";
-import fetchUser from "../../utils/fetchUser";
+import { EditableCredentials } from "../../interfaces/user";
+import Link from "next/link";
 
 export default function EditAccount({ session }: any) {
   const [loading, setLoading] = useState(true);
   const [username, setUsername] = useState("");
   const [bio, setBio] = useState("");
   const [avatar_url, setAvatarUrl] = useState("");
-  const [user, setUser] = useState<User>({
-    username: "",
-    bio: "",
-    avatar_url: "",
-    balance: 300,
-  });
 
   useEffect(() => {
-    fetchUser(session, setUser);
+    getProfile();
   }, [session]);
 
-  async function updateProfile({ username, bio, avatar_url, balance }: User) {
+  async function getProfile() {
     try {
       setLoading(true);
-      const user: any = supabase.auth.user();
+      const user = supabase.auth.user()!;
+
+      let { data, error, status } = await supabase
+        .from("profiles")
+        .select(`username, bio, avatar_url`)
+        .eq("id", user.id)
+        .single();
+
+      if (error && status !== 406) {
+        throw error;
+      }
+
+      if (data) {
+        setUsername(data.username);
+        setBio(data.bio);
+        setAvatarUrl(data.avatar_url);
+      }
+    } catch (error: any) {
+      alert(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  async function updateProfile({
+    username,
+    bio,
+    avatar_url,
+  }: EditableCredentials) {
+    try {
+      setLoading(true);
+      const user = supabase.auth.user()!;
 
       const updates = {
         id: user.id,
         username,
         bio,
         avatar_url,
-        balance,
-        updated_at: new Date(),
       };
 
       let { error } = await supabase.from("profiles").upsert(updates, {
@@ -49,53 +72,51 @@ export default function EditAccount({ session }: any) {
   }
 
   return (
-    <div className="w-[50%] mx-auto bg-dark text-center">
-      <Avatar
-        url={avatar_url}
-        size={150}
-        onUpload={(url: string) => {
-          setAvatarUrl(url);
-          updateProfile({ username, bio, avatar_url: url });
-        }}
-      />
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          className="text-2xl border-2 outline-none my-4 p-4"
-          id="email"
-          type="text"
-          value={session.user.email}
-          disabled
+    <div className="py-16">
+      <div className="w-[90%] md:w-[50%] mx-auto bg-dark text-center py-8 rounded-xl">
+        <Avatar
+          url={avatar_url}
+          size={120}
+          onUpload={(url: string) => {
+            setAvatarUrl(url);
+            updateProfile({ username, bio, avatar_url: url });
+          }}
         />
-      </div>
-      <div>
-        <label htmlFor="username">Name</label>
-        <input
-          className="text-dark text-2xl border-2 outline-none my-4 p-4"
-          id="username"
-          type="text"
-          value={username || ""}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="bio">Bio</label>
-        <textarea
-          className="text-dark text-2xl border-2 outline-none my-4 p-4"
-          id="bio"
-          value={bio || ""}
-          onChange={(e) => setBio(e.target.value)}
-        />
-      </div>
+        <p>{session.user.email}</p>
 
-      <div>
-        <button
-          className="submit"
-          onClick={() => updateProfile({ username, bio, avatar_url })}
-          disabled={loading}
-        >
-          {loading ? "Loading ..." : "Update"}
-        </button>
+        <div className="my-4">
+          <div className="labeled-input">
+            <label htmlFor="username">Username</label>
+            <input
+              className="text-xl mb-4 p-4"
+              id="username"
+              type="text"
+              value={username || ""}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="labeled-input">
+            <label htmlFor="bio">Bio</label>
+            <textarea
+              id="bio"
+              value={bio || ""}
+              onChange={(e) => setBio(e.target.value)}
+            />
+          </div>
+
+          <div className="buttons">
+            <button className="submit">
+              <Link href="/profile">Back</Link>
+            </button>
+            <button
+              className="submit"
+              onClick={() => updateProfile({ username, bio, avatar_url })}
+              disabled={loading}
+            >
+              {loading ? "Loading ..." : "Update"}
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
