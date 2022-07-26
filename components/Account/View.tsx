bfcbd675bb640fc120b01a802cd840f6609260d4
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-07-26 23:04:34 +0600

    feat(components): display more data on profile

--- components/Account/View.tsx
+++ components/Account/View.tsx
@@ -5,9 +5,13 @@ import User from "../../interfaces/user";
 import fetchUser from "../../utils/fetchUser";
 import Link from "next/link";
 import { supabase } from "../../utils/supabaseClient";
+import relativeTime from "../../utils/relativeTime";
+import Image from "next/image";
 
 export default function ViewAccount({ session }: any) {
   const [user, setUser] = useState<User>({
+    id: "",
+    updated_at: new Date(),
     username: "",
     bio: "",
     avatar_url: "",
@@ -20,17 +24,24 @@ export default function ViewAccount({ session }: any) {
 
   return (
     <div className="py-16">
-      <div className="w-[90%] md:w-[50%] mx-auto bg-dark text-center py-8 rounded-xl">
+      <div className="w-[90%] md:w-[50%] mx-auto bg-dark text-center py-8 rounded-xl z-1">
         <Avatar url={user.avatar_url} size={120} />
         <h1 className="mb-2 text-xl sm:text-4xl">{user.username}</h1>
+        <p>Joined {relativeTime(user.updated_at)}</p>
         <h3 className="text-left mx-4 sm:mx-16">Bio</h3>
         <div className="mx-4 sm:mx-16 bg-input bg-opacity-[50%] h-[128px] px-1 rounded-md text-left">
           <p className="pt-2 pl-2">{user.bio}</p>
         </div>
-        <h3 className="text-left sm:mx-16 my-2 mx-4">
-          Balance: {user.balance}
+        <h3 className="flex items-center text-left sm:mx-16 my-2 mx-4">
+          Balance: {user.balance}{" "}
+          <Image
+            src="/images/features/jazbapoint.png"
+            width={36}
+            height={36}
+            alt="Economy Feature"
+          />
         </h3>
-        <div className="flex sm:flex-row flex-col sm:mx-16">
+        <div className="buttons">
           <button className="submit">
             <Link href="/profile/edit/">Edit</Link>
           </button>
