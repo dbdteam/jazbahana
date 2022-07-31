eabf1e9404c65d420c4922cdbfa323ecbad51f62
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-07-31 19:18:03 +0600

    feat(components): adapt search to dark theme

--- src/components/Main/Search.tsx
+++ src/components/Main/Search.tsx
@@ -2,12 +2,12 @@ import { IoSearch } from "react-icons/io5";
 
 export default function Search() {
   return (
-    <div className="hidden sm:flex items-center bg-gray-100 rounded-full">
-      <IoSearch className="m-4 text-dark" width={20} height={20} />
+    <div className="hidden sm:flex items-center bg-gray-700 rounded-full">
+      <IoSearch className="m-4" width={20} height={20} />
       <input
-        className="outline-none text-dark bg-gray-100 mr-4"
+        className="outline-none mr-4 bg-gray-700"
         type="text"
-        placeholder="Search Notes, Rooms..."
+        placeholder="Search for rooms..."
       />
     </div>
   );
