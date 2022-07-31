0bb08a9b43839f12c98f1d3375fbbb79ef98265e
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-08-01 00:47:02 +0600

    feat(components): make navbar dynamic

--- src/components/Main/Navbar.tsx
+++ src/components/Main/Navbar.tsx
@@ -1,8 +1,22 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { supabase } from "../../lib/supabaseClient";
 import Search from "./Search";
 
 const Navbar = () => {
+  const { push } = useRouter();
+  const [session, setSession] = useState<any>(null);
+
+  useEffect(() => {
+    setSession(supabase.auth.session());
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
   return (
     <nav className="bg-dark border-b-2">
       <ul className="flex items-center justify-between px-8 py-2">
@@ -16,14 +30,20 @@ const Navbar = () => {
           />
         </Link>
         <Search />
-        <div className="flex text-xl sm:text-2xl gap-4">
-          <li>
-            <Link href="/profile">profile</Link>
-          </li>
-          <li className="text-primary">
-            <Link href="/rooms">rooms</Link>
-          </li>
-        </div>
+        {session ? (
+          <div className="flex text-xl sm:text-2xl gap-4">
+            <li>
+              <Link href="/profile">profile</Link>
+            </li>
+            <li className="text-primary">
+              <Link href="/rooms">rooms</Link>
+            </li>
+          </div>
+        ) : (
+          <div className="flex text-xl sm:text-2xl">
+            <button onClick={() => push("/profile/edit")}>sign in</button>
+          </div>
+        )}
       </ul>
     </nav>
   );
