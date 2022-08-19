8364010cbed41311b5b2d10802fb6375f6509c99
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-08-19 20:01:35 +0600

    feat(search): light theme support

--- src/components/Layout/Search.tsx
+++ src/components/Layout/Search.tsx
@@ -2,10 +2,10 @@ import { IconSearch } from "@supabase/ui";
 
 export default function Search() {
   return (
-    <div className="hidden sm:flex items-center bg-gray-700 rounded-full">
+    <div className="hidden sm:flex items-center bg-gray-200 dark:bg-gray-700 rounded-full">
       <IconSearch className="m-4" width={20} height={20} />
       <input
-        className="outline-none mr-4 bg-gray-700"
+        className="outline-none mr-4 bg-gray-200 dark:bg-gray-700"
         type="text"
         placeholder="Search for rooms..."
       />
